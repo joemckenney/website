236a5ba7224fe343fbafe56dc6f74319a2a98f18
Joe McKenney <joe@crowprose.com>
2025-11-22 12:50:23 -0800

    fix: formatting and remove generated code

--- apps/@auth/sdk/src/index.ts
+++ apps/@auth/sdk/src/index.ts
@@ -1,3 +1,3 @@
+export { client } from "./generated/client.gen.js";
 export * from "./generated/sdk.gen.js";
 export * from "./generated/types.gen.js";
-export { client } from "./generated/client.gen.js";
